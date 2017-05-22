ac936a27ba70f41e04703695d6d2f560507abd1e
guilin <guilin1_happy@163.com>
2017-05-22 10:21:47 +0800

    修改运行脚本

--- src/main/java/org/dataalgorithms/chap01/spark/run-groupbykey2.sh
+++ src/main/java/org/dataalgorithms/chap01/spark/run-groupbykey2.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 export SPARK_HOME=/app/spark1.5
-export SPARK_MASTER=spark://ubuntu:7077
+export YARN_MASTER=yarn-cluster
 BOOK_HOME=/opt/data/ideaprojects/data-algorithms
 APP_JAR=$BOOK_HOME/target/data-algorithms-1.0-SNAPSHOT.jar
 INPUT=/data/secondary_sort/input/timeseries.txt
@@ -10,9 +10,10 @@ $HADOOP_HOME/bin/hdfs dfs -rm -r $OUTPUT
 prog=org.dataalgorithms.chap01.spark.SecondarySortUsingGroupByKey2
 $SPARK_HOME/bin/spark-submit \
     --class $prog \
-    --master $SPARK_MASTER \
-    --executor-memory 2G \
-    --total-executor-cores 20 \
+    --master $YARN_MASTER \
+    --num-executors 3 \
+    --driver-memory 1g \
+    --executor-memory 1g --executor-cores 1 \
     $APP_JAR \
     $INPUT $OUTPUT
 
